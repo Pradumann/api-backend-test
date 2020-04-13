e43f90caf0cd046803353ea2984f64361b17a916
Praduman rapria <praduman47@gmail.com>
2020-04-13 16:34:19 +0530

    fixing user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,6 @@ const UserProfile = mongoose.model('UserProfile')
 const verifyToken = require('../middleware/requireToken')
 
 router.post('/saveUserProfile', verifyToken, async (req, res) => {
-  const { email, name, username } = req.body
-  console.log('email = ' + email + ' username = ', username)
   return res.status(200).send('Your email is awesome')
 })
 
