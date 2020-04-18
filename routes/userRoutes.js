34f0fa61583d770215fd4f02f1989818ea9efd96
Praduman rapria <praduman47@gmail.com>
2020-04-18 11:01:37 +0530

    Upload profile dummy endpoin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -59,21 +59,27 @@ router.post('/updateUserProfile', verifyToken, async (req, res) => {
 })
 
 router.post('/uploadProfilePicture', verifyToken, async (req, res) => {
-  res.status(200).send({
-    message: 'Profile picture updated',
-    picture: ''
-  })
-  // try {
-  //   return res.status(200).send({
-  //     message: 'Profile picture updated',
-  //     picture: ''
-  //   })
-  // } catch (e) {
-  //   return res.status(422).send({
-  //     error: e,
-  //     message: 'Failed to upload profile picutre because ' + e.message
-  //   })
-  // }
+  try {
+    const { email, name, username } = req.body
+    const profile = await Profile.updateOne(
+      {
+        email: email
+      },
+      {
+        name: name,
+        username: username
+      }
+    )
+    return res.status(200).send({
+      message: 'Update successful',
+      profile: req.body
+    })
+  } catch (error) {
+    return res.status(422).send({
+      error: error,
+      message: 'Failed to update user profile because ' + error.message
+    })
+  }
 })
 
 module.exports = router
